e9ae7510f00242523cae4aa74f6837dad000dd1b
Riquelmy Arcanjo de Araújo <riquelmyaaraujo@gmail.com>
2021-04-18 16:23:53 -0300

    Ajustando input modal

--- dist/masks.js
+++ dist/masks.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#telefone").mask("(00) 0000-0000");
     $("#cpf").mask("000.000.000-00");
     $("#celular").mask("(00) 00000-0000");
+
+    $("#cpfModal").mask("000.000.000-00");
+    $("#telefoneModal").mask("(00) 0000-0000");
 })
