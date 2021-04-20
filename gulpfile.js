3fea0791c2f1017896816d086a40c6255de8dc9f
Riquelmy Arcanjo de Araújo <riquelmyaaraujo@gmail.com>
2021-04-20 13:22:28 -0300

    Incluindo AutoPrefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 const image = require('gulp-image');
+const autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
     src: 'src/app.scss',
@@ -12,12 +13,13 @@ var sassDevOptions = {
 }
 
 //Tasks
-gulp.task('styles', function(){
+gulp.task('stylesAutoPrefixer', function(){
     return gulp.src(paths.src)
     .pipe(sass(sassDevOptions).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
+    .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest(paths.dist));
 });
-
  
 /*gulp.task('reduceImg', function () {
   gulp.src('src/img/*')
@@ -26,12 +28,12 @@ gulp.task('styles', function(){
 });*/
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.scss', gulp.series('styles'));
+    gulp.watch(paths.src, gulp.series('stylesAutoPrefixer'));
     //posso add + aqui
 });
 
 
 // Default Task
-gulp.task('default', gulp.parallel('styles', /*'reduceImg',*/'watch'));
+gulp.task('default', gulp.series('stylesAutoPrefixer', /*'reduceImg',*/'watch'));
 
 
